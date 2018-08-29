d1a7850ae3687acce2811123db31386e135c44bf
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-08-29 15:31:54 +0100

    remove unused import

--- invest/models.py
+++ invest/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from wagtail.api import APIField
 from wagtail.admin.edit_handlers import FieldPanel, ObjectList, MultiFieldPanel
-from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 from directory_constants.constants import cms
 
