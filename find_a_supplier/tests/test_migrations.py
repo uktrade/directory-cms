eb91b9039a31e86b09bf5fa33673009da4b292c9
richtier <rikatee@gmail.com>
2018-08-30 10:23:30 +0100

    Populate service name in historic slug

--- find_a_supplier/tests/test_migrations.py
+++ find_a_supplier/tests/test_migrations.py
@@ -1,6 +1,8 @@
 import pytest
 
+from core.models import HistoricSlug
 from find_a_supplier.tests import factories
+from find_a_supplier import models
 
 
 @pytest.mark.skip('slow')
@@ -43,3 +45,31 @@ def test_populate_breadcrumb(migration, settings):
     assert breadcrumb.label_pt == 'breadcrumb [pt]'
     assert breadcrumb.label_pt_br == 'breadcrumb [pt-br]'
     assert breadcrumb.label_ar == 'breadcrumb [ar]'
+
+
+@pytest.mark.skip(reason='slow')
+@pytest.mark.django_db
+def test_populate_service_name(migration, settings):
+    page = factories.IndustryContactPageFactory.create()
+    models.IndustryContactPage.objects.filter(
+        pk=page.pk
+    ).update(service_name=None)
+    HistoricSlug.objects.all().delete()
+
+    page.refresh_from_db()
+
+    assert page.service_name is None
+    assert page.historicslug_set.all().count() == 0
+
+    migration.before([('find_a_supplier', '0065_auto_20180829_1027')])
+
+    apps = migration.apply('find_a_supplier', '0066_auto_20180830_0632')
+
+    page = apps.get_model(
+        'find_a_supplier', 'IndustryContactPage'
+    ).objects.get(pk=page.pk)
+
+    assert page.service_name == 'FIND_A_SUPPLIER'
+    assert apps.get_model('core', 'HistoricSlug').objects.filter(
+        page=page, service_name='FIND_A_SUPPLIER'
+    ).count() == 1
--- invest/tests/test_migrations.py
+++ invest/tests/test_migrations.py
@@ -1,6 +1,8 @@
 import pytest
 
 from invest.tests import factories
+from invest import models
+from core.models import HistoricSlug
 
 
 @pytest.mark.skip(reason='slow')
@@ -26,3 +28,31 @@ def test_populate_historic_slug_service_name(migration, settings):
     page.refresh_from_db()
 
     assert page.service_name == 'INVEST'
+
+
+@pytest.mark.skip(reason='slow')
+@pytest.mark.django_db
+def test_populate_service_name(migration, settings):
+    page = factories.SetupGuideLandingPageFactory.create()
+    models.SetupGuideLandingPage.objects.filter(
+        pk=page.pk
+    ).update(service_name=None)
+    HistoricSlug.objects.all().delete()
+
+    page.refresh_from_db()
+
+    assert page.service_name is None
+    assert page.historicslug_set.all().count() == 0
+
+    migration.before([('invest', '0012_merge_20180829_1421')])
+
+    apps = migration.apply('invest', '0013_auto_20180830_0632')
+
+    page = apps.get_model(
+        'invest', 'SetupGuideLandingPage'
+    ).objects.get(pk=page.pk)
+
+    assert page.service_name == 'INVEST'
+    assert apps.get_model('core', 'HistoricSlug').objects.filter(
+        page=page, service_name='INVEST'
+    ).count() == 1
