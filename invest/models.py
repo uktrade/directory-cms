fe8439bbccac0d8d08701d28a44a9e36e5267255
richtier <rikatee@gmail.com>
2018-09-14 14:41:13 +0100

    Fix HPO view live urls

--- invest/models.py
+++ invest/models.py
@@ -932,7 +932,7 @@ class HighPotentialOpportunityFormPage(ExclusivePageMixin, BasePage):
     ]
 
     service_name_value = cms.INVEST
-    view_path = 'high-potential-opportunities/'
+    view_path = 'high-potential-opportunities/rail/contact/'
     slug_identity = 'high-potential-opportunity-form'
 
     heading = models.CharField(max_length=255)
@@ -1441,7 +1441,7 @@ class HighPotentialOpportunityDetailPage(BasePage):
 
 class HighPotentialOpportunityFormSuccessPage(BasePage):
     service_name_value = cms.INVEST
-    view_path = 'high-potential-opportunities/'
+    view_path = 'high-potential-opportunities/rail/contact/'
     slug_identity = 'high-potential-opportunity-submit-success'
 
     breadcrumbs_label = models.CharField(max_length=50)
