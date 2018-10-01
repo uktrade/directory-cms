275b2c64c128f84fbcb266462730a0bb48090e86
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-10-01 13:24:23 +0100

    increase codecoverage

--- export_readiness/tests/test_views.py
+++ export_readiness/tests/test_views.py
@@ -110,3 +110,16 @@ def test_homepage(admin_client, root_page):
     assert response.status_code == 200
     assert 'articles' in response.json()
     assert len(response.json()['articles']) == 5
+
+
+def test_homepage_no_news(admin_client, root_page):
+
+    home_page = factories.HomePageFactory.create(
+        parent=root_page
+    )
+
+    url = reverse('api:pages:detail', kwargs={'pk': home_page.pk})
+    response = admin_client.get(url)
+    assert response.status_code == 200
+    assert 'articles' in response.json()
+    assert len(response.json()['articles']) == 0
