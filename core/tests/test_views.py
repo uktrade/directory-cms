fbd99bd22668bc633c19651c63a8c7801f37d4b9
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-10-04 15:04:42 +0100

    add missing parameter test

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -509,5 +509,12 @@ def test_lookup_by_tag_slug(admin_client, root_page):
         url,
         {'tag_slug': tag.slug}
     )
-    import ipdb; ipdb.set_trace()
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_lookup_by_tag_slug_missing_param(admin_client):
+    url = reverse('lookup-by-tag-list')
+    response = admin_client.get(url)
+    assert response.status_code == 400
+    assert response.json() == {'tag_slug parameter is required'}
