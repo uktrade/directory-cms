c919c2dc7147702e9347b7d7473eb40e47f81eb3
kowalcj0 <kowalczykjanusz@gmail.com>
2019-02-05 17:54:13 +0000

    clean up var names

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -44,6 +44,7 @@ def test_normal_user_with_no_group_assigned_sees_no_groups():
     assert form.fields['groups'].queryset.count() == 0
 
 
+@pytest.mark.CMS_837
 @pytest.mark.django_db
 def test_branch_moderators_should_see_pages_only_from_their_branch(branch_moderator_factory):
     listing_1, article_1, _, _, client_1 = branch_moderator_factory.get()
@@ -110,7 +111,7 @@ def test_branch_moderators_cannot_access_pages_from_other_branch(branch_moderato
 @pytest.mark.CMS_839
 @pytest.mark.django_db
 def test_editors_can_create_child_pages(branch_editor_factory):
-    listing_1, article_1, _, _, client_1 = branch_editor_factory.get()
+    listing, article, _, _, client = branch_editor_factory.get()
     data = {
         "article_title": "test article",
         "article_teaser": "test article",
@@ -119,49 +120,49 @@ def test_editors_can_create_child_pages(branch_editor_factory):
         "slug": "test-article",
     }
 
-    resp_1 = client_1.post(
+    resp_1 = client.post(
         reverse('wagtailadmin_pages:add',
                 args=(
-                    article_1._meta.app_label,
-                    article_1._meta.model_name,
-                    listing_1.pk,
+                    article._meta.app_label,
+                    article._meta.model_name,
+                    listing.pk,
                 )),
         data=data,
     )
-
     assert resp_1.status_code == status.HTTP_302_FOUND, (
         f"Something went wrong: {resp_1.context['form'].errors}"
     )
 
+    # check if new page is visible in the "Pages" menu
     new_article_id = int(resp_1.url.split('/')[3])  # format is "/admin/pages/6/edit/"
-    resp_2 = client_1.get(
-        f'/admin/api/v2beta/pages/?child_of={listing_1.pk}&for_explorer=1'
+    resp_2 = client.get(
+        f'/admin/api/v2beta/pages/?child_of={listing.pk}&for_explorer=1'
     )
     assert resp_2.status_code == status.HTTP_200_OK
     assert resp_2.json()['meta']['total_count'] == 2
-    assert resp_2.json()['items'][0]['id'] == article_1.pk
+    assert resp_2.json()['items'][0]['id'] == article.pk
     assert resp_2.json()['items'][1]['id'] == new_article_id
 
 
 @pytest.mark.CMS_839
 @pytest.mark.django_db
 def test_editors_cant_create_child_pages_without_mandatory_data(branch_editor_factory):
-    listing_1, article_1, _, _, client_1 = branch_editor_factory.get()
+    listing, article, _, _, client = branch_editor_factory.get()
     mandatory_fields = {
         "article_title", "article_teaser", "article_body_text", "title_en_gb", "slug"
     }
     data = {}
-    res = client_1.post(
+    resp = client.post(
         reverse('wagtailadmin_pages:add',
                 args=(
-                    article_1._meta.app_label,
-                    article_1._meta.model_name,
-                    listing_1.pk,
+                    article._meta.app_label,
+                    article._meta.model_name,
+                    listing.pk,
                 )),
         data=data,
     )
-    assert res.status_code == status.HTTP_200_OK
-    assert not (mandatory_fields - set(res.context['form'].errors.keys()))
+    assert resp.status_code == status.HTTP_200_OK
+    assert not (mandatory_fields - set(resp.context['form'].errors.keys()))
 
 
 @pytest.mark.CMS_839
@@ -177,7 +178,7 @@ def test_editors_cant_create_pages_in_branch_they_dont_manage(branch_editor_fact
         "slug": "test-article",
     }
 
-    response = client_1.post(
+    resp = client_1.post(
         reverse('wagtailadmin_pages:add',
                 args=(
                     article_2._meta.app_label,
@@ -186,27 +187,27 @@ def test_editors_cant_create_pages_in_branch_they_dont_manage(branch_editor_fact
                 )),
         data=data,
     )
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert resp.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.CMS_839
 @pytest.mark.django_db
 def test_editors_cannot_publish_child_pages(branch_editor_factory):
-    listing_1, article_1, _, user_1, client_1 = branch_editor_factory.get()
+    listing, _, _, user, client = branch_editor_factory.get()
 
-    draft_page = exred_factories.ArticlePageFactory(parent=listing_1, live=False)
-    revision = draft_page.save_revision(user=user_1, submitted_for_moderation=True)
+    draft_page = exred_factories.ArticlePageFactory(parent=listing, live=False)
+    revision = draft_page.save_revision(user=user, submitted_for_moderation=True)
 
-    resp_1 = client_1.post(
+    resp = client.post(
         reverse('wagtailadmin_pages:approve_moderation', args=[revision.pk])
     )
-    assert resp_1.status_code == status.HTTP_403_FORBIDDEN
+    assert resp.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.CMS_839
 @pytest.mark.django_db
 def test_editors_can_submit_changes_for_moderation(branch_editor_factory):
-    listing_1, article_1, _, user_1, client_1 = branch_editor_factory.get()
+    listing, article, _, _, client = branch_editor_factory.get()
     data = {
         "article_title": "new title",
         "article_teaser": "new teaser",
@@ -214,19 +215,19 @@ def test_editors_can_submit_changes_for_moderation(branch_editor_factory):
         "title_en_gb": "next title",
         "action-submit": "Submit for moderation",  # this action triggers notification
     }
-    resp_1 = client_1.post(
-        reverse('wagtailadmin_pages:edit', args=[article_1.pk]),
+    resp = client.post(
+        reverse('wagtailadmin_pages:edit', args=[article.pk]),
         data=data,
     )
     # on success, user should be redirected on parent page listing
-    assert resp_1.status_code == status.HTTP_302_FOUND, resp_1.context['form'].errors
-    assert int(resp_1.url.split('/')[3]) == listing_1.pk  # format is /admin/pages/3/
+    assert resp.status_code == status.HTTP_302_FOUND, resp.context['form'].errors
+    assert int(resp.url.split('/')[3]) == listing.pk  # format is /admin/pages/3/
 
 
 @pytest.mark.CMS_839
 @pytest.mark.django_db
 def test_editors_can_view_drafts(branch_editor_factory):
-    listing_1, article_1, _, user_1, client_1 = branch_editor_factory.get()
+    _, article, _, _, client = branch_editor_factory.get()
     data = {
         "article_title": "new title",
         "article_teaser": "new teaser",
@@ -236,35 +237,35 @@ def test_editors_can_view_drafts(branch_editor_factory):
     }
 
     # Create a draft and stay on the same admin page
-    resp_1 = client_1.post(
-        reverse('wagtailadmin_pages:edit', args=[article_1.pk]),
+    resp_1 = client.post(
+        reverse('wagtailadmin_pages:edit', args=[article.pk]),
         data=data,
     )
     assert resp_1.status_code == status.HTTP_302_FOUND
     assert "has been updated" in resp_1.context['message']
-    assert int(resp_1.url.split('/')[3]) == article_1.pk  # format is /admin/pages/3/edit/
+    assert int(resp_1.url.split('/')[3]) == article.pk  # format is /admin/pages/3/edit/
 
     # Viewing draft will redirect user to the application site
-    resp_2 = client_1.get(
-        reverse('wagtailadmin_pages:view_draft', args=[article_1.pk])
+    resp_2 = client.get(
+        reverse('wagtailadmin_pages:view_draft', args=[article.pk])
     )
     assert resp_2.status_code == status.HTTP_302_FOUND
-    assert article_1.slug in resp_2.url
+    assert article.slug in resp_2.url
 
 
 @pytest.mark.CMS_839
 @pytest.mark.django_db
 def test_editors_can_list_revisions(branch_editor_factory):
-    listing_1, article_1, _, user_1, client_1 = branch_editor_factory.get()
+    _, article, _, user, client = branch_editor_factory.get()
 
-    revision = article_1.save_revision(user=user_1, submitted_for_moderation=True)
-    revert_path = f'/admin/pages/{article_1.pk}/revisions/{revision.pk}/revert/'
+    revision = article.save_revision(user=user, submitted_for_moderation=True)
+    revert_path = f'/admin/pages/{article.pk}/revisions/{revision.pk}/revert/'
 
-    resp_2 = client_1.get(
-        reverse('wagtailadmin_pages:revisions_index', args=[article_1.pk])
+    resp = client.get(
+        reverse('wagtailadmin_pages:revisions_index', args=[article.pk])
     )
-    assert resp_2.status_code == status.HTTP_200_OK
-    assert revert_path in resp_2.content.decode()
+    assert resp.status_code == status.HTTP_200_OK
+    assert revert_path in resp.content.decode()
 
 
 @pytest.mark.CMS_839
