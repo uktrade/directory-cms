acd37bbc6e5c02ba23feeeae1914e8dd14465b3d
Jess Gilbert <jesgil@softwire.com>
2019-03-18 15:29:13 +0000

    HPO Title and intro added to cms

--- invest/serializers.py
+++ invest/serializers.py
@@ -194,6 +194,8 @@ class InvestHomePageSerializer(BasePageSerializer):
     subsections = serializers.SerializerMethodField()
     sector_title = serializers.CharField(max_length=255)
     sector_intro = serializers.CharField(max_length=255)
+    hpo_title = serializers.CharField(max_length=255)
+    hpo_intro = serializers.CharField(max_length=255)
     sector_button_text = serializers.CharField(max_length=255)
     setup_guide_title = serializers.CharField(max_length=255)
     setup_guide_content = core_fields.MarkdownToHTMLField()
