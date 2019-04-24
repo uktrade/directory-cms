331160dc7b3798a3f469fe69c85627f8a8bb42e7
Unknown <jessica.gilbert@softwire.com>
2019-04-24 16:04:12 +0100

    Removed now unused fields for capital investment section

--- invest/serializers.py
+++ invest/serializers.py
@@ -188,10 +188,6 @@ class InvestHomePageSerializer(BasePageSerializer):
     benefits_section_img = wagtail_fields.ImageRenditionField('original')
     capital_invest_section_title = serializers.CharField(max_length=255)
     capital_invest_section_content = core_fields.MarkdownToHTMLField()
-    capital_invest_section_cta_text = serializers.CharField(max_length=255)
-    capital_invest_section_cta_url = serializers.CharField(max_length=255)
-    capital_invest_section_background_image = \
-        wagtail_fields.ImageRenditionField('original')
     eu_exit_section_title = serializers.CharField(max_length=255)
     eu_exit_section_content = core_fields.MarkdownToHTMLField()
     eu_exit_section_call_to_action_text = serializers.CharField(max_length=255)
