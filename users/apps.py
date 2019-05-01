28edd2caf8d56d79af4b46b197ea161765898f36
Karl Hobley <karl@kaed.uk>
2019-05-01 11:18:04 +0100

    [CMS-1446] Prevent users from changing their email address

--- users/apps.py
+++ users/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        # HACK: Unregister "change email" account menu item hook
+        from wagtail.core import hooks
+        from wagtail.admin.wagtail_hooks import register_account_change_email
+        hooks.search_for_hooks()
+        registered_hooks = hooks._hooks.get('register_account_menu_item')
+        if registered_hooks:
+            registered_hooks.remove((register_account_change_email, 0))
+            hooks._hooks['register_account_menu_item'] = registered_hooks
