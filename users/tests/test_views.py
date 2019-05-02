7a14f0ea2329bbe05193bb4269e03baa9a74b67a
Karl Hobley <karl@kaed.uk>
2019-05-02 12:09:59 +0100

    Additional tests to improve coverage

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -48,6 +48,15 @@ def test_create_user_view_invalid_form(admin_client):
 
 
 @pytest.mark.django_db
+def test_get_edit_user_view(admin_client):
+    user = UserFactory(username='test', email='test@test.com')
+    url = reverse('wagtailusers_users:edit', kwargs={'pk': user.pk})
+    response = admin_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.context['can_delete'] is True
+
+
+@pytest.mark.django_db
 def test_edit_user_view(admin_client):
     user = UserFactory(username='test', email='test@test.com')
     url = reverse('wagtailusers_users:edit', kwargs={'pk': user.pk})
@@ -88,3 +97,25 @@ def test_edit_user_view_cant_change_basic_details(admin_client):
     assert response.url == reverse('wagtailusers_users:index')
     user.refresh_from_db()
     assert user.username == 'test'
+    assert user.is_active is True
+
+
+@pytest.mark.django_db
+def test_edit_user_view_cant_reactivate_disabled_users(admin_client):
+    user = UserFactory(username='test', email='test@test.com', is_active=False)
+    url = reverse('wagtailusers_users:edit', kwargs={'pk': user.pk})
+    response = admin_client.post(
+        url,
+        data={
+            'username': 'foobar',
+            'email': 'test@test.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'is_active': 'on',
+            'groups': ['1']
+        }
+    )
+    assert response.status_code == status.HTTP_302_FOUND
+    assert response.url == reverse('wagtailusers_users:index')
+    user.refresh_from_db()
+    assert user.is_active is False
