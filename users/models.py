ded10820937bc69c552e78408a1cf2b8e55cfac4
Andy Babic <andyjbabic@gmail.com>
2019-05-07 16:16:10 +0100

    Re-add new fields

--- users/models.py
+++ users/models.py
@@ -13,10 +13,26 @@ class UserProfile(models.Model):
         (APPROVED, APPROVED)
     )
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
     assignment_status = models.CharField(
         choices=STATUSES,
         default=CREATED,
         max_length=30
+    )
+
+    self_assigned_group = models.ForeignKey(
+        'auth.Group',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    team_leader = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='team_leader_for',
+        null=True,
+        on_delete=models.SET_NULL,
     )
