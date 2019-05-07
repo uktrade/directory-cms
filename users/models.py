f6405f2c509d77670f512fdef498eee21876edaa
Andy Babic <andyjbabic@gmail.com>
2019-05-07 14:39:32 +0100

    Add new fields to support workflow

--- users/models.py
+++ users/models.py
@@ -16,8 +16,22 @@ ASSIGNMENT_CHOICES = (
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                  on_delete=models.CASCADE)
+
     assignment_status = models.CharField(
         choices=ASSIGNMENT_CHOICES,
         default=CREATED,
         max_length=30
+    )
+
+    self_assigned_group = models.ForeignKey(
+        'auth.Group',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    team_leader = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='team_leader_for',
+        null=True,
+        on_delete=models.SET_NULL
     )
