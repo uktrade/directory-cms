92c5c0a9adace4b1cab0c95c386988a36ecc8d03
Andy Babic <andyjbabic@gmail.com>
2019-05-07 09:13:39 +0100

    Add some Select2 form widgets for use in the new form

--- core/widgets.py
+++ core/widgets.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from django.utils.safestring import mark_safe
 from wagtail.utils.widgets import WidgetWithScript
 
 
@@ -24,3 +25,38 @@ class MarkdownTextarea(WidgetWithScript, forms.widgets.Textarea):
                 'wagtailadmin/js/page-chooser-modal.js',
             )
         )
+
+
+class Select2WidgetMediaMixin:
+    class Media:
+        css = {
+            'all': (
+                'core/js/select2/select2.css',
+                'core/js/select2/select2-wagtailadmin.css'
+            )
+        }
+        js = ('core/js/select2/select2.min.js',)
+
+
+class Select2Widget(Select2WidgetMediaMixin, forms.Select):
+
+    def render(self, name, value, attrs=None):
+        html = super().render(name, value, attrs)
+        extra = mark_safe('\n'.join((
+            "<script> $(function () {",
+            "    $('select[name=\"{}\"]').select2();".format(name),
+            "});</script>"
+        )))
+        return html + extra
+
+
+class Select2MultipleWidget(Select2WidgetMediaMixin, forms.SelectMultiple):
+
+    def render(self, name, value, attrs=None):
+        html = super().render(name, value, attrs)
+        extra = mark_safe('\n'.join((
+            "<script>$(function () {",
+            "    $('select[name=\"{}\"]').select2();".format(name),
+            "});</script>"
+        )))
+        return html + extra
