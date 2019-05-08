e4bd13c28289d266ce2e54496943ba958a75a86b
Andy Babic <andyjbabic@gmail.com>
2019-05-08 21:53:56 +0100

    Fix a few things in middleware

--- users/middleware.py
+++ users/middleware.py
@@ -14,22 +14,23 @@ class SSORedirectUsersToRequestAccessViews:
     def process_request(self, request):
 
         user = request.user
+        profile = user.userprofile
 
         if not user.is_authenticated() or user.is_superuser:
             # allow the view to handle these
             return
 
-        if not request.url.startswith(self.admin_root_url):
+        if not request.path.startswith(str(self.admin_root_url)):
             # not an 'admin' url, so no need to intervene
             return
 
         # no need to intervene if these views are already being accessed
         for url in self.ignore_admin_urls:
-            if request.url == url:
+            if request.path == str(url):
                 return
 
-        if user.profile.assignment_status == UserProfile.STATUS_CREATED:
+        if profile.assignment_status == UserProfile.STATUS_CREATED:
             return redirect('wagtailusers_users:sso_request_access')
 
-        if user.profile.assignment_status == UserProfile.STATUS_AWAITING_APPROVAL: # noqa
+        if profile.assignment_status == UserProfile.STATUS_AWAITING_APPROVAL:
             return redirect('wagtailusers_users:sso_request_access_success')
