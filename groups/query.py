15a9485d973cd9a6ee0a984e2ded194a0074261f
Andy Babic <andyjbabic@gmail.com>
2019-05-08 15:02:28 +0100

    Remove unused GroupQuerySet methods

--- groups/query.py
+++ groups/query.py
@@ -7,22 +7,6 @@ class GroupInfoQuerySet(models.QuerySet):
     def with_groups(self):
         return self.select_related('group')
 
-    def visible_to_anyone(self):
-        return self.filter(visibility=self.model.VISIBILITY_UNRESTRICTED)
-
-    def visible_to_managers(self):
-        return self.filter(visibility__in=(
-            self.model.VISIBILITY_UNRESTRICTED,
-            self.model.VISIBILITY_MANAGERS_ONLY,
-        ))
-
-    def visible_to_user(self, user):
-        if user.is_superuser:
-            return self.all()
-        if user.has_perm(change_user_perm):
-            return self.visible_to_managers()
-        return self.visible_to_anyone()
-
     @property
     def team_leaders_group(self):
         """Returns a single object"""
