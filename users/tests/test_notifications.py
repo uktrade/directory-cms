dab38a05bc4bb3db22bb2311b103c3be42a29278
Andy Babic <andyjbabic@gmail.com>
2019-05-13 10:13:27 +0100

    Use autospec=True when mocking send_email_notification()

--- users/tests/test_notifications.py
+++ users/tests/test_notifications.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 from users.notifications import (
+    notifications_client,
     notify_user_of_access_approval,
     notify_team_leader_of_access_request,
 )
@@ -12,7 +13,14 @@ def test_notify_user_of_access_approval(rf):
         user_email='jon.snow@winterfell.gov.uk',
         reviewer_name='Ygritte',
     )
-    with patch('users.notifications.notifications_client.send_email_notification') as mocked_method: # noqa
+
+    # patching with autospect=True so that incorrect argument names
+    # will raise errors
+    with patch.object(
+        notifications_client,
+        'send_email_notification',
+        autospec=True
+    ) as mocked_method:
         notify_user_of_access_approval(request, **kwargs)
 
     expected_call_args = dict(
@@ -39,7 +47,13 @@ def test_notify_team_leader_of_access_request(rf):
         user_role='Knight',
     )
 
-    with patch('users.notifications.notifications_client.send_email_notification') as mocked_method: # noqa
+    # patching with autospect=True so that incorrect argument names
+    # will raise errors
+    with patch.object(
+        notifications_client,
+        'send_email_notification',
+        autospec=True
+    ) as mocked_method:
         notify_team_leader_of_access_request(request, **kwargs)
 
     expected_call_args = dict(
