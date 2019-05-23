0b156474c9f630ada7f2283ec39c42dee196f29a
Andy Babic <andyjbabic@gmail.com>
2019-05-23 17:37:15 +0100

    Add sso views to their own module and include them in the main url conf using a different namespace

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
 
 from wagtail.users.views import users as original_users_views
 
@@ -10,18 +8,6 @@ app_name = 'wagtailusers_users'
 urlpatterns = [
     url(r'^$', original_users_views.index, name='index'),
     url(r'^add/$', views.CreateUserView.as_view(), name='add'),
-    url(
-        r'^request-access/$',
-        login_required(views.SSORequestAccessView.as_view()),
-        name="sso_request_access",
-    ),
-    url(
-        r'^request-access/thanks/$',
-        login_required(TemplateView.as_view(
-            template_name="sso/request_access_success.html"
-        )),
-        name="sso_request_access_success",
-    ),
     url(r'^(?P<pk>\d+)/$', views.EditUserView.as_view(), name='edit'),
     url(r'^([^\/]+)/delete/$', original_users_views.delete, name='delete'),
 ]
---
+++ users/urls_sso.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+from wagtail.users.views import users as original_users_views
+
+from . import views
+
+app_name = 'sso'
+urlpatterns = [
+    url(
+        r'^$',
+        login_required(views.SSORequestAccessView.as_view()),
+        name="request_access",
+    ),
+    url(
+        r'^thanks/$',
+        login_required(TemplateView.as_view(
+            template_name="sso/request_access_success.html"
+        )),
+        name="request_access_success",
+    ),
+]
