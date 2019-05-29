9f8f2d4903441bab85f1e4b566e301059bd3c07a
George Hickman <george@ghickman.co.uk>
2019-05-29 16:54:41 +0100

    Replace standard moderation panel to display new moderation queue objects
    Wagtail's default PagesForModerationPanel looks at PageRevision objects.
    We want to expose our new PagePendingModeration objects so this
    replaces that panel with a new one, maintaining the default ordering.

--- moderation_queue/wagtail_hooks.py
+++ moderation_queue/wagtail_hooks.py
@@ -1,6 +1,9 @@
+from django.template.loader import render_to_string
 from django.urls import reverse
 from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
+
+from .models import PagePendingModeration
 
 
 @hooks.register('register_admin_menu_item')
@@ -11,3 +14,24 @@ def add_moderation_queue_to_menu():
         classnames='icon icon-folder-inverse',
         order=600,
     )
+
+
+class ModerationQueuePanel:
+    order = 200
+
+    def render(self):
+        pending_moderations = (PagePendingModeration.objects.order_by('-created_at')
+                                                            .select_related('revision', 'user'))
+        context = {'pending_moderations': pending_moderations}
+        return render_to_string('moderation_queue/panel.html', context=context)
+
+
+@hooks.register('construct_homepage_panels')
+def replace_moderation_panel(request, panels):
+    for i, panel in enumerate(panels):
+        if panel.name != 'pages_for_moderation':
+            continue
+
+        # replace standard Wagtail Moderation panel (PagesForModerationPanel)
+        # with a custom panel to show PagePendingModeration objects.
+        panels[i] = ModerationQueuePanel()
