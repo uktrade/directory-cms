85a1b8872966bc0aec375a90eaa244b72e733dc9
George Hickman <george@ghickman.co.uk>
2019-05-29 13:16:29 +0100

    Add ModerationQueue model
    This models a User requesting moderation on a PageReview object.

--- moderation_queue/models.py
+++ moderation_queue/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class PagePendingModeration(models.Model):
+    revision = models.ForeignKey('wagtailcore.PageRevision', verbose_name=_('revision'), on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), verbose_name=_('user'), on_delete=models.SET_NULL, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} requested moderation of "{self.revision.page}" at {self.created_at}'
