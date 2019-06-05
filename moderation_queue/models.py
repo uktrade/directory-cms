38e8ffbdb7eb5e253e39d804b54534c901310d46
George Hickman <george@ghickman.co.uk>
2019-06-05 08:22:43 +0100

    Add reivew comments

--- moderation_queue/models.py
+++ moderation_queue/models.py
@@ -13,17 +13,24 @@ class Moderation(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
+    objects = ModerationQuerySet.as_manager()
+
     def __str__(self):
         return f'{self.revision.user} requested moderation of "{self.revision.page}" at {self.created_at}'
 
     def is_2i_moderated(self):
         return self.reviews.filter(is_accepted=True).exists()
 
+    @property
+    def latest_review(self):
+        return self.reviews.latest('created_at')
+
 
 class ModeratorReview(models.Model):
     moderation = models.ForeignKey('Moderation', verbose_name=_('moderation'), on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(get_user_model(), verbose_name=_('user'), on_delete=models.SET_NULL, null=True)
     is_accepted = models.BooleanField(verbose_name=_('accept page'), default=False)
+    comment = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
 
