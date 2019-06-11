4b16c710399ad7405749174632ca80b18052c71e
Unknown <jessica.gilbert@softwire.com>
2019-06-11 10:55:24 +0100

    Removed unused import

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from core import fields
-from great_international.models import CapitalInvestOpportunityPage
 
 
 class BasePageSerializer(serializers.Serializer):
