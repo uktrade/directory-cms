7bd608d87dfd61bd44d8c0d3b3504255e402de95
Andy Babic <andyjbabic@gmail.com>
2019-06-14 17:40:36 +0100

    Upgrade to Django 2.2.2

--- core/tests/helpers.py
+++ core/tests/helpers.py
@@ -1,6 +1,17 @@
 import requests
 
 
+def clean_post_data(post_data):
+    """
+    Removes None values from data, so that it can posted to Django's test
+    client without TypeError being raised in Django 2.2+
+    """
+    return {
+        key: value for key, value in post_data.items()
+        if value is not None
+    }
+
+
 def create_response(status_code, json_body={}, content=None):
     response = requests.Response()
     response.status_code = status_code
