e3b7b3c1747aada49090f89bbb5f6923f8f74817
Unknown <jessica.gilbert@softwire.com>
2019-06-17 13:46:32 +0100

    Removed unecessary test

--- great_international/tests/test_models.py
+++ great_international/tests/test_models.py
@@ -71,16 +71,6 @@ def test_models_hierarchy():
     ]
 
 
-@pytest.mark.parametrize('model', [
-    models.CapitalInvestOpportunityListingPage,
-])
-def test_opp_listing_required_translatable_fields(model):
-    assert model.get_required_translatable_fields() == [
-        'breadcrumbs_label',
-        'search_results_title'
-    ]
-
-
 @pytest.mark.django_db
 def test_article_inherit_tags_from_parent(root_page):
     tag1 = exread_factories.TagFactory(name='foo')
