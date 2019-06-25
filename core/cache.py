40ea3baf5580811acf877a1e7c582a1520e2191b
Andy Babic <andyjbabic@gmail.com>
2019-06-25 15:15:55 +0100

    Fix incorrect slug reference

--- core/cache.py
+++ core/cache.py
@@ -19,7 +19,7 @@ from conf.celery import app
 
 ROOT_PATHS_TO_SERVICE_NAMES = {
     slugs.GREAT_HOME: cms.EXPORT_READINESS,
-    slugs.GREAT_INTERNATIONAL_HOME: cms.GREAT_INTERNATIONAL,
+    slugs.GREAT_HOME_INTERNATIONAL: cms.GREAT_INTERNATIONAL,
     'find-a-supplier-app': cms.FIND_A_SUPPLIER,
     'invest-app': cms.INVEST,
     'components-app': cms.COMPONENTS,
