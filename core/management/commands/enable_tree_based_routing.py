ac42c2b1a4700f9b613ae9e3646474c7db7fd23e
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2019-07-15 11:35:43 +0100

    use dictionary as choices

--- core/management/commands/enable_tree_based_routing.py
+++ core/management/commands/enable_tree_based_routing.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
         parser.add_argument(
             '--servicename',
             help='Select international or domestic',
-            choices=['international', 'domestic']
+            choices=SERVICES_MAPPING
         )
 
     @no_translations
