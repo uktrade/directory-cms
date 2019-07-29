43ab1e77376161157c6bc05ce154d7e2d92c5f85
eadpearce <e.pearce@tutanota.com>
2019-07-29 11:44:43 +0100

    Add remaining fields changes from invest models

--- great_international/translation.py
+++ great_international/translation.py
@@ -607,14 +607,14 @@ class InvestHomePageTranslation(BaseTranslationOptions):
         'hero_call_to_action_text',
         'hero_call_to_action_url',
 
+        'teaser',
+
         'benefits_section_title',
         'benefits_section_intro',
         'benefits_section_content',
+        'benefits_section_cta_text',
+        'benefits_section_cta_url',
         'benefits_section_img',
-
-        'capital_invest_section_title',
-        'capital_invest_section_content',
-        'capital_invest_section_image',
 
         'eu_exit_section_title',
         'eu_exit_section_content',
@@ -626,6 +626,10 @@ class InvestHomePageTranslation(BaseTranslationOptions):
         'sector_intro',
         'hpo_title',
         'hpo_intro',
+
+        'capital_invest_section_title',
+        'capital_invest_section_content',
+        'capital_invest_section_image',
         'setup_guide_title',
         'setup_guide_content',
         'setup_guide_img',
@@ -634,6 +638,22 @@ class InvestHomePageTranslation(BaseTranslationOptions):
         'isd_section_image',
         'isd_section_title',
         'isd_section_text',
+
+        'featured_card_one_image',
+        'featured_card_one_title',
+        'featured_card_one_summary',
+        'featured_card_one_cta_link',
+
+        'featured_card_two_image',
+        'featured_card_two_title',
+        'featured_card_two_summary',
+        'featured_card_two_cta_link',
+
+        'featured_card_three_image',
+        'featured_card_three_title',
+        'featured_card_three_summary',
+        'featured_card_three_cta_link',
+
         'how_we_help_title',
         'how_we_help_lead_in',
         # how we help
