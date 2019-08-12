18ad9b48d6a26278ebe97ad06323fc039925d694
Alessandro De Noia <alessandro.denoia@gmail.com>
2019-08-12 11:18:58 +0100

    Add article pages as child pages of international sectors

--- tests/great_international/test_models.py
+++ tests/great_international/test_models.py
@@ -76,6 +76,7 @@ def test_models_hierarchy():
         ]
     assert great_international.InternationalSectorPage.allowed_subpage_models() == [
             great_international.InternationalSubSectorPage,
+            great_international.InternationalArticlePage
         ]
     assert great_international.AboutDitLandingPage.allowed_subpage_models() == [
             great_international.AboutDitServicesPage
