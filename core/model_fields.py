5f6a6057cbf94c168fcb073eeb2be4222e39de78
eadpearce <e.pearce@tutanota.com>
2019-08-22 10:54:33 +0100

    Remove slug hyperlink validators from markdown fields

--- core/model_fields.py
+++ core/model_fields.py
@@ -4,12 +4,6 @@ from core import validators as core_validators, widgets
 
 
 class MarkdownField(TextField):
-    def __init__(self, validators=None, *args, **kwargs):
-        validators = validators or []
-        if core_validators.slug_hyperlinks not in validators:
-            validators.append(core_validators.slug_hyperlinks)
-        super().__init__(validators=validators, *args, **kwargs)
-
     def formfield(self, **kwargs):
         defaults = {
             'widget': widgets.MarkdownTextarea
