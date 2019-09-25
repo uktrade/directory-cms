5bb724e687bf92a05d418ffc0ade40f6a9e1e0fa
JessGilbert <jessica.gilbert@digital.trade.gov.uk>
2019-09-25 17:07:24 +0100

    Fixed test

--- tests/great_international/test_serializers.py
+++ tests/great_international/test_serializers.py
@@ -1418,7 +1418,7 @@ def test_new_int_home_page_has_related_how_dit_help_pages(
 
     home = InternationalHomePageFactory(
         slug='international',
-        related_how_dit_help_pages=[how_we_help],
+        related_how_dit_help_one=how_we_help,
         parent=international_root_page
     )
 
