4332492b9be515dacaa4e37ea30cf50cd24aaa9c
Alessandro <alessandro.denoia@digital.trade.gov.uk>
2019-10-15 12:00:42 +0100

    small changes to the campaing link

--- export_readiness/blocks_serializers.py
+++ export_readiness/blocks_serializers.py
@@ -8,7 +8,8 @@ from core import fields as core_fields
 class CampaignBlockStreamChildSerializer(StreamChildBaseSerializer):
     heading = serializers.CharField()
     subheading = serializers.CharField()
-    related_link = serializers.CharField()
+    related_link_text = serializers.CharField()
+    related_link_url = serializers.CharField()
     image = wagtail_fields.ImageRenditionField('original')
     video = core_fields.VideoField()
     video_transcript = serializers.CharField()
