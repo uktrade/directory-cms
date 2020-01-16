7cb58d7e2560eaa11b0cb6adc5e8e14c6fb0c409
Alessandro <alessandro.denoia@digital.trade.gov.uk>
2020-01-16 16:34:01 +0000

    add cache and simply serializer
    remove unused select_related
    
    fix tests

--- conf/settings.py
+++ conf/settings.py
@@ -166,7 +166,6 @@ API_CACHE_EXPIRE_SECONDS = env.int(
     'API_CACHE_EXPIRE_SECONDS', 60 * 60 * 24 * 30  # 30 days
 )
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
