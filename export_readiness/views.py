1255028be51689d696baea6179856a2b1aabd6c7
Richard Tier <contact@richardtier.co.uk>
2020-01-20 13:34:43 +0000

    Update views.py

--- export_readiness/views.py
+++ export_readiness/views.py
@@ -5,7 +5,6 @@ from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.generics import RetrieveAPIView, ListAPIView
-from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 from conf.signature import SignatureCheckPermission
