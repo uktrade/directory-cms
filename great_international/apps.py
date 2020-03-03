23b055664a7273e06f3d69ea2b675e6ff852f782
richtier <contact@richardtier.co.uk>
2020-03-03 11:50:49 +0000

    Remove region aware cache

--- great_international/apps.py
+++ great_international/apps.py
@@ -3,10 +3,3 @@ from django.apps import AppConfig
 
 class GreatInternationalConfig(AppConfig):
     name = 'great_international'
-
-    def ready(self):
-        from core.cache import DatabaseCacheSubscriber, RegionAwareDatabaseCacheSubscriber
-        from great_international import cache
-
-        for subscriber in cache.subscribers:
-            subscriber.subscribe()
