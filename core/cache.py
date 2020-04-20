f6d2fc88bd7921c567ada4aeb300a65d35c1f8c1
richtier <contact@richardtier.co.uk>
2020-04-20 16:04:34 +0100

    Add cache supoort to draft pages

--- core/cache.py
+++ core/cache.py
@@ -131,7 +131,10 @@ class CachePopulator:
                     )
                     if instance.has_unpublished_changes:
                         draft_instance = instance.get_latest_nested_revision_as_page()
-                        draft_serializer = serializer_class(instance=draft_instance)
+                        draft_serializer = serializer_class(
+                            instance=draft_instance,
+                            context={'is_draft': True}
+                        )
                         page_cache.set(
                             page_id=instance.id,
                             data=draft_serializer.data,
