3b5a256406e7541978c7f11ad183ac0bc40d4bac
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2021-10-13 16:42:35 +0100

    Added hero_video for how we can help page

--- great_international/models/investment_atlas.py
+++ great_international/models/investment_atlas.py
@@ -495,6 +495,13 @@ class InvestmentGeneralContentPage(
         blank=True,
         help_text='Main page hero image, above the opportunity title'
     )
+    hero_video = models.ForeignKey(
+        'wagtailmedia.Media',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
     strapline = models.CharField(
         max_length=200,
         blank=False,
