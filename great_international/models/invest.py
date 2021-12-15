438ddeee5d1e60fb9985efbe4a2f81dd696a9a7d
Fred Marecesche <fred@froddd.co.uk>
2021-12-15 16:45:25 +0000

    Remove unused constants

--- great_international/models/invest.py
+++
@@ -1,8 +0,0 @@
-VIDEO_TRANSCRIPT_HELP_TEXT = (
-    "If the video is present, a transcript must be provided."
-)
-
-IMAGE_DESCRIPTION_HELP_TEXT = (
-    "If this image adds extra information to the page that is not already provided by the text "
-    "(e.g. if the image is a diagram, chart, or has text on it) then an image description must be provided."
-)
