43f7a1caa4921237346535eb8935c18ca8964bf0
davidu1975 <david.urquhart@digital.trade.gov.uk>
2023-06-15 15:28:30 +0100

    move tests

---
+++ tests/factories.py
@@ -0,0 +1,39 @@
+import factory
+import factory.fuzzy
+import wagtail_factories
+
+from core import snippets
+
+
+class TagFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = snippets.Tag
+
+    name = factory.fuzzy.FuzzyText(length=10)
+
+
+class IndustryTagFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = snippets.IndustryTag
+
+    name = factory.fuzzy.FuzzyText(length=10)
+    icon = factory.SubFactory(wagtail_factories.ImageFactory)
+
+
+class RegionFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = snippets.Region
+
+    name = factory.fuzzy.FuzzyText(length=10)
+
+
+class CountryFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = snippets.Country
+
+    name = factory.fuzzy.FuzzyText(length=10)
+    slug = factory.fuzzy.FuzzyText(length=10)
