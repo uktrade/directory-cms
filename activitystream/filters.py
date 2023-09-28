de088535153bf7537c9b576a331cdd3b015d0332
Timothy Patterson <tim.patterson@digital.trade.gov.uk>
2023-09-28 16:45:47 +0100

    increase codecov, flatten contents of object key in WagtailPageSerializer

--- activitystream/filters.py
+++ activitystream/filters.py
@@ -1,4 +1,5 @@
 from django_filters import NumberFilter, FilterSet
+from wagtail.models import Page
 
 
 class ActivityStreamCMSContentFilter(FilterSet):
@@ -7,3 +8,7 @@ class ActivityStreamCMSContentFilter(FilterSet):
     def filter_after(self, queryset, name, value):
         value = value or 0
         return queryset.filter(id__gt=value)
+
+    class Meta:
+        model = Page
+        fields = ['id']
--- activitystream/urls.py
+++ activitystream/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-# from great_components.decorators import skip_ga360
-
 import activitystream.views
 
 app_name = 'activitystream'
