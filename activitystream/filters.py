fd1418529326450f589120856b4f6b1506df680f
Timothy Patterson <tim.patterson@digital.trade.gov.uk>
2023-10-04 11:40:25 +0100

    use char filter for pagination

--- activitystream/filters.py
+++ activitystream/filters.py
@@ -1,12 +1,12 @@
-from django_filters import NumberFilter, FilterSet
+from django_filters import CharFilter, FilterSet
 from wagtail.models import Page
 
 
 class ActivityStreamCMSContentFilter(FilterSet):
-    after = NumberFilter(method='filter_after')
+    after = CharFilter(method='filter_after')
 
     def filter_after(self, queryset, name, value):
-        value = value or 0
+        value = value or '0'
         return queryset.filter(id__gt=value)
 
     class Meta:
